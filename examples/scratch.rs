1f6e48189b009173796c88449046ead9d0b92474
folkengine <gaoler@electronicpanopticon.com>
2022-04-17 07:23:13 -0700

    Added config files

---
+++ examples/scratch.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("{}", u8::MAX);
+    println!("{}", 0b11111111);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+#![warn(clippy::pedantic)]
+
+pub type WinCount = u8;
+
+struct Flag;
+
+impl Flag {
+    const FIRST: u8 = 0b0000_0001;
+    const SECOND: u8 = 0b0000_0010;
+    const THIRD: u8 = 0b0000_0100;
+    const FORTH: u8 = 0b0000_1000;
+    const FIFTH: u8 = 0b0001_0000;
+    const SIXTH: u8 = 0b0010_0000;
+    const SEVENTH: u8 = 0b0100_0000;
+    const EIGHT: u8 = 0b1000_0000;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
